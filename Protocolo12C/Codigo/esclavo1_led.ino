c03a3aa8aea90037d325d760904504a85db36901
Benja-S-V <l231000161@mazatlan.tecnm.mx>
2026-09-22 15:45:28 -0700

    Create esclavo1_led.ino

---
+++ Protocolo12C/Codigo/esclavo1_led.ino
@@ -0,0 +1,17 @@
+#include <Wire.h>
+const uint8_t PIN_LED = 13;
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  Wire.begin(0x08);
+  Wire.onReceive(recibir);
+}
+
+void loop() { }
+
+void recibir(int cuantos) {
+  while (Wire.available()) {
+    byte dato = Wire.read();
+    digitalWrite(PIN_LED, dato ? HIGH : LOW);
+  }
+}
